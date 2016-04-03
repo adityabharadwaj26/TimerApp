46092949d03eeab57e6866a58ec1ed5999093fc4
ADITYA BHARADWAJ <adityabharadwaj26@gmail.com>
2016-04-03 19:09:26 +0530

    Final Design
    This is the final design, the timer now runs for 20 minutes, the
    progress bar moves along with the timer and at 20 minutes a alarm tone
    plays. Which can be stopped by reset.

--- app/src/main/java/com/example/aditya/timerapp/MainActivity.java
+++ app/src/main/java/com/example/aditya/timerapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.aditya.timerapp;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -8,15 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button startButton;
-    private Button pauseButton;
-    private Button resetButton;
     private ProgressBar progress;
     private TextView timerValue;
     private long startTime = 0L;
@@ -25,160 +22,83 @@ public class MainActivity extends AppCompatActivity {
     long timeSwapBuff = 0L;
     long updatedTime = 0L;
     int progressStatus = 0;
-    private boolean stopped = false;
+    private Ringtone ring;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        final CountDown timer = new CountDown();
+        //final CountDown timer = new CountDown();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timerValue = (TextView) findViewById(R.id.timerVal);
-        startButton = (Button) findViewById(R.id.startButton);
+        Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-            timer.start();
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+
             }
         });
-        pauseButton = (Button) findViewById(R.id.stopButton);
+        Button pauseButton = (Button) findViewById(R.id.pauseButton);
         pauseButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                try {
-                    timer.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
             }
         });
-        resetButton = (Button) findViewById(R.id.resetButton);
+        Button resetButton = (Button) findViewById(R.id.resetButton);
         resetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                timer.cancel();
+                timeInMilliseconds = 0L;
+                timeSwapBuff = 0L;
+                updatedTime = 0L;
+                int secs = (int) (updatedTime / 1000);
+                int mins = secs / 60;
+                secs = secs % 60;
+                int milliseconds = (int) (updatedTime % 1000);
+                timerValue.setText(mins + ":"
+                        + String.format("%02d", secs) + ":"
+                        + String.format("%03d", milliseconds));
+                customHandler.removeCallbacks(updateTimerThread);
+                stopTimer();
+
             }
         });
         progress = (ProgressBar) findViewById(R.id.progressBar);
-
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        // client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-
-    public void stopRun() {
-        //if (updatedTime == 0) {
-            //Toast.makeText(new MainActivity(), "StopRun",Toast.LENGTH_LONG).show();
-        timeSwapBuff = 0;
-        timerValue.setText("00:00:000");
-        //updateTimerThread.
-        customHandler.removeCallbacks(updateTimerThread);
-        //}
-    }
-
     private Runnable updateTimerThread = new Runnable() {
+
         @Override
         public void run() {
-            //long totalMilliseconds = 1500000;
-            //while (!stopped){
-                long totalMilliseconds = 15000;
-                updatedTime = totalMilliseconds - SystemClock.currentThreadTimeMillis();
-                int secs = (int) (updatedTime / 1000);
-                int mins = secs / 60;
-                int hour = mins / 60;
-                secs = secs % 60;
-                int milliseconds = (int) (updatedTime % 1000);
-                progressStatus = (int) (totalMilliseconds - updatedTime);
-
-                //while(stopped == false) {
-                    timerValue.setText(String.format("%02d", hour) + ":" + String.format("%02d", mins) + ":"
-                            + String.format("%02d", secs) + ":"
-                            + String.format("%03d", milliseconds));
-                //}
-
-                progress.setMax((int) totalMilliseconds);
-                progress.setProgress(progressStatus);
-                customHandler.postDelayed(this, 0);
-                if (updatedTime == 0) {
-                    //Toast.makeText(new MainActivity(), "Void Stop call",Toast.LENGTH_LONG).show();
-                    //stopRun();
-                    stopped = true;
-                } else
-                    stopped = false;
-            }
-        //}
-    };
-
-    public void stop() {
-        //Toast toast = new Toast()
-        Toast.makeText(new MainActivity(), "Void Stop",Toast.LENGTH_LONG).show();
-        customHandler.removeCallbacks(updateTimerThread);
-    }
-
-    /*@Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.aditya.timerapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.aditya.timerapp/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }*/
-
-    /*private Runnable updateTimerThread = new Runnable() {
-
-        public void run() {
-
+            int maxTime = 1200000;
             timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
             updatedTime = timeSwapBuff + timeInMilliseconds;
-            int secs = (int) (updatedTime / 1000);
+
+            int secs = ((int) (updatedTime / 1000));
             int mins = secs / 60;
-            int hour = mins / 60;
-            int pomodoro = 2;
             secs = secs % 60;
-            if (mins == pomodoro){
-                timerValue.setTextColor(Color.BLUE);
-
-            }
             int milliseconds = (int) (updatedTime % 1000);
-            progressStatus = (int) secs;
-            int stat = progressStatus / 60;
-
-            timerValue.setText(String.format("%02d", hour) + ":" + String.format("%02d", mins) + ":"
-                            + String.format("%02d", secs) + ":"
-                            + String.format("%03d", milliseconds));
-
-            progress.setMax(pomodoro);
-            progress.setProgress((stat / pomodoro)* 100);
+            timerValue.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));
+            progress.setMax(maxTime);
+            progress.setProgress((int) updatedTime);
             customHandler.postDelayed(this, 0);
+            if (mins == 20 && secs == 0) {
+                playTimer();
+            }
         }
-    };*/
+    };
+
+    public void playTimer(){
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        ring = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        ring.play();
+        timeSwapBuff += timeInMilliseconds;
+        customHandler.removeCallbacks(updateTimerThread);
+    }
+    public void stopTimer(){
+        ring.stop();
+    }
 }
+
