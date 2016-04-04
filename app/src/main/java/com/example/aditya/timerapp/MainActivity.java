db4cebc6d360114471c89f94af4352417c0896be
ADITYA BHARADWAJ <adityabharadwaj26@gmail.com>
2016-04-04 22:29:00 +0530

    Minor 3

--- app/src/main/java/com/example/aditya/timerapp/MainActivity.java
+++ app/src/main/java/com/example/aditya/timerapp/MainActivity.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +30,24 @@ public class MainActivity extends AppCompatActivity {
     int maxTime = 1200000;
     private Ringtone ring;
     boolean pause = false;
+    boolean maxTimeset = false;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    Toolbar mActionBarToolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         //final CountDown timer = new CountDown();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mActionBarToolbar.setTitle("Pomodoro");
+        setSupportActionBar(mActionBarToolbar);
+
         timerValue = (TextView) findViewById(R.id.timerVal);
         final Button startButton = (Button) findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
                     customHandler.postDelayed(updateTimerThread, 0);
                     startButton.setText(R.string.Pause);
                     pause = true;
-                } else{
+                } else {
                     timeSwapBuff += timeInMilliseconds;
                     customHandler.removeCallbacks(updateTimerThread);
                     startButton.setText(R.string.Start);
@@ -61,14 +78,19 @@ public class MainActivity extends AppCompatActivity {
                 timerValue.setText(mins + ":"
                         + String.format("%02d", secs) + ":"
                         + String.format("%03d", milliseconds));
+                progress.setProgress((int) updatedTime);
                 customHandler.removeCallbacks(updateTimerThread);
-                if (updatedTime == maxTime) {
+                if (maxTimeset == true) {
                     stopTimer();
                 }
             }
         });
         progress = (ProgressBar) findViewById(R.id.progressBar);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     private Runnable updateTimerThread = new Runnable() {
         @Override
         public void run() {
@@ -85,19 +107,63 @@ public class MainActivity extends AppCompatActivity {
             progress.setMax(maxTime);
             progress.setProgress((int) updatedTime);
             customHandler.postDelayed(this, 0);
-            if (mins == 20 && secs == 0) {
+            if (updatedTime == maxTime) {
+                maxTimeset = true;
                 playTimer();
+                //stopTimer();
             }
         }
     };
-    public void playTimer(){
+
+    public void playTimer() {
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         ring = RingtoneManager.getRingtone(getApplicationContext(), notification);
         ring.play();
         timeSwapBuff += timeInMilliseconds;
         customHandler.removeCallbacks(updateTimerThread);
     }
-    public void stopTimer(){
+
+    public void stopTimer() {
         ring.stop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.aditya.timerapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.aditya.timerapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
